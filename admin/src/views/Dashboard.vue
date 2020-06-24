fec75f61681339a73789c971ad238aed85b7785b
Stephen <traders.com.tw@gmail.com>
2020-06-24 14:55:18 +0800

    20200624-2

--- admin/src/views/Dashboard.vue
+++ admin/src/views/Dashboard.vue
@@ -1,66 +1,30 @@
 <template>
    <v-container>
-         <core-html-editor ref="htmlEditor" :content="content"
-         @changed="onContentChanged"
-         />
-
-         <v-layout row>
-            <v-flex xs12>
-               <v-btn @click.prevent="submit" color="success">確定</v-btn>
-            </v-flex>
-         </v-layout>
+      <core-date-filter :start="start" :end="end" />
    </v-container>
   
 </template>
 
 <script>
-import axios from 'axios';
 import { mapState, mapGetters } from 'vuex';
-import { MANUAL } from '@/consts'; 
 import { DIALOG_MAX_WIDTH } from '@/config';
 
 export default {
    data () {
       return {
-
-         references: {},
-         content: `<p>
-         “黑人弗洛伊德之死”再一次撕开了美国种族歧视的疮疤。对于美国民众来说，这又是一次或根治种族问题的机会，于是他们奋力呼喊、极力反抗，由此引发的抗议潮自上月26日以来已持续12天时间并蔓延至全美。
-         
-         
-         </p>
-         
-         <p>
-         从白宫到金门大桥，从费城到纽约。当地时间6月6日，各地的抗议活动迎来了新高潮：首都华盛顿爆发最大规模抗议活动，市长本人也走到抗议者中间；全美抗议人数创下新高，纽约、旧金山、洛杉矶、费城等地也有成千上万人走上街头。
-          <UPLOADPHOTO>20200109/57339dd5-1a70-4558-90fb-f0d19f4455ed.png&width=420<\/UPLOADPHOTO>
-          
-          </p>
-
-         `
+         start: '2020-05-01',
+         end: ''
       }
    },
    computed: {
-      ...mapGetters(['contentMaxWidth']),
-      htmlEditor() {
-			if(this.$refs.htmlEditor) return this.$refs.htmlEditor;
-			else if (this.references.htmlEditor) return this.references.htmlEditor;
-			return null;
-      }
+      ...mapGetters(['contentMaxWidth'])
    },
 	beforeMount() {
-      let name = '';
-      console.log(name.split('/'));
+      
    },
-   mounted() {
-		this.references = { ...this.$refs };
-	},
 	methods: {
-      onContentChanged() {
-         console.log('onContentChanged');
-      },
       submit() {
-         let text = this.htmlEditor.getContent();
-         console.log(text);
+         
       }
 	}
 }
